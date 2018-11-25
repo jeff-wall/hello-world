792d2d44776b18f80b2b101e959169173ee562b1
jeff-wall <35283357+jeff-wall@users.noreply.github.com>
2018-11-25 18:01:40 +0800

    Update utility.h
    add class of 'i_event'

--- utility.h
+++ utility.h
@@ -5,10 +5,24 @@ protected :
 public :
     virtual bool lock() = 0 ;
     virtual void unlock() = 0 ;
-
 	const std::string & cr_name ;
-	i_mutex() : cr_name(m_str_name) {}
+i_mutex() : cr_name(m_str_name) {}
     virtual ~i_mutex()
 {}
+}
+;
+
+
+class i_event
+{
+protected :
+	std::string m_str_name ;
+public :
+    virtual void notify() = 0 ;
+    virtual bool wait(UI4 wait_ms = 0) = 0 ;
+	const std::string & cr_name ;
+	i_event() : cr_name(m_str_name) {}
+    virtual ~i_event()
+    {}
 }
 ;
